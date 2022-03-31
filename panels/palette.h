853b25fc773c7ee21e6780bbb6b8b483eb8c0589
nltrieuxuyen27 <xuyennguyen2733@gmail.com>
2022-03-31 15:20:27 -0600

    Included <QVector> in header file Moved defaultPalette initialization to palette's constructor in palette.cpp

--- panels/palette.h
+++ panels/palette.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QSize>
+#include <QVector>
 
 class Palette : public QObject
 {
@@ -12,25 +13,8 @@ public:
 private:
     QSize paletteSize;
     // TODO: maybe put this into a .hex file in the res folder and read from there
-    QVector<QString> defaultPalette
-    {
-        "1a1c2c",
-        "5d275d",
-        "b13e53",
-        "ef7d57",
-        "ffcd75",
-        "a7f070",
-        "38b764",
-        "257179",
-        "29366f",
-        "3b5dc9",
-        "41a6f6",
-        "73eff7",
-        "f4f4f4",
-        "94b0c2",
-        "566c86",
-        "333c57"
-    };
+    QVector<QString> defaultPalette;
+
 public slots:
     void paletteColumnsChanged(int);
     void paletteRowsChanged(int);
